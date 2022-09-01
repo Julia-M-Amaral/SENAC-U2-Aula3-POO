2c150b33707c31693aa52af1154c31b102902ed7
Julia Mangabeira <JuliaAmaralSantos@outlook.com>
2022-09-01 20:29:21 -0300

    Criação de caixa de diálogo com JOptionPane

---
+++ Main/src/main/java/com/mycompany/main/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.main;
+
+import javax.swing.JOptionPane;
+import javax.swing.Icon;
+/**
+ *
+ * @author jubsz
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Icon icone = new javax.swing.ImageIcon("C:\\Users\\jubsz\\OneDrive\\IMAGENS/1b781d1b53444b12c260d225e2fa7c4b.jpg");
+        JOptionPane.showMessageDialog(null, "Olá. \n Esse é um exemplo de um método polimórfico.", "Informações Gerais", JOptionPane.ERROR_MESSAGE, icone);
+    }
+}
+    
